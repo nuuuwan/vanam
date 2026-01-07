114d3919a2bd59d8ea4ac630c24442555620ab7e
nuuuwan <nuuuwan@gmail.com>
2026-01-07 14:33:30 +0530

    feat: add Karla font to theme typography in App component

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const theme = createTheme({
     },
   },
   typography: {
-    fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+    fontFamily: '"Karla", "Roboto", "Helvetica", "Arial", sans-serif',
   },
 });
 
