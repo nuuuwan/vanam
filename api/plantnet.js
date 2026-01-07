e731be2ab0d21533dad9465776eef852d7b90877
nuuuwan <nuuuwan@gmail.com>
2026-01-07 11:45:39 +0530

    testing only

--- api/plantnet.js
+++ api/plantnet.js
@@ -1,23 +1,6 @@
-export default async function handler(req, res) {
-  if (req.method !== "POST") {
-    return res.status(405).json({ error: "POST only" });
-  }
-
-  try {
-    const response = await fetch(
-      `https://my-api.plantnet.org/v2/identify/all?api-key=${process.env.PLANTNET_KEY}`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(req.body),
-      }
-    );
-
-    const data = await response.json();
-    res.status(200).json(data);
-  } catch (err) {
-    res.status(500).json({ error: "PlantNet request failed" });
-  }
+export default function handler(req, res) {
+  res.status(200).json({
+    keyPresent: !!process.env.PLANTNET_KEY,
+    keyValue: process.env.PLANTNET_KEY || null,
+  });
 }
