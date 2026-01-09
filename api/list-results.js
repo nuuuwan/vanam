3bf1cfbfebb2657d81b969da0d25ab2a41f56f73
nuuuwan <nuuuwan@gmail.com>
2026-01-09 09:27:43 +0530

    fix: remove unnecessary blank line in list-results API handler

--- api/list-results.js
+++ api/list-results.js
@@ -43,7 +43,7 @@ export default async function handler(req, res) {
     const jsonOldFormatBlobs = oldFormatBlobs.filter((blob) =>
       blob.pathname.endsWith(".json")
     );
-    
+
     const oldFormatPhotos = await Promise.all(
       jsonOldFormatBlobs.map(async (blob) => {
         try {
