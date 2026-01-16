820aa973b6c6fece8fdbca9600ec2740333aae48
nuuuwan <nuuuwan@gmail.com>
2026-01-16 15:24:07 +0530

    feat: implement Cache class for local storage management

---
+++ src/nonview/base/Cache.js
@@ -0,0 +1,58 @@
+export default class Cache {
+  static SALT = "v1";
+  static CACHE_DURATION_MS = 10 * 60 * 1000;
+  static LOCAL_CACHE = {};
+  static LOCAL_CACHE_SIZE = 0;
+
+  static async get(key, callback) {
+    const roundedTimestamp = Math.floor(Date.now() / this.CACHE_DURATION_MS);
+    const cacheKey = `${key}-${roundedTimestamp}-${this.SALT}`;
+
+    if (Cache.LOCAL_CACHE[cacheKey] !== undefined) {
+      return Cache.LOCAL_CACHE[cacheKey];
+    }
+
+    try {
+      const cachedValue = localStorage.getItem(cacheKey);
+      if (cachedValue !== null) {
+        return JSON.parse(cachedValue);
+      }
+    } catch (error) {
+      console.error(`Error reading from cache for key "${cacheKey}":`, error);
+    }
+
+    const value = await callback();
+
+    try {
+      const payload = JSON.stringify(value);
+      const payloadSize = payload.length;
+      localStorage.setItem(cacheKey, payload);
+      Cache.LOCAL_CACHE[cacheKey] = value;
+      Cache.LOCAL_CACHE_SIZE += payloadSize;
+
+      if (Cache.LOCAL_CACHE_SIZE > 5 * 1_000_000 || payloadSize > 100_000) {
+        console.warn(
+          `⚠️[Cache] ${(payloadSize / 1_000_000.0).toFixed(3)}MB/${(
+            Cache.LOCAL_CACHE_SIZE / 1_000_000.0
+          ).toFixed(1)}MB: "${cacheKey}"`
+        );
+      }
+    } catch (error) {
+      console.error(`Error writing to cache for key "${cacheKey}":`, error);
+      localStorage.clear();
+    }
+
+    return value;
+  }
+
+  static clear(key) {
+    if (key) {
+      localStorage.removeItem(key);
+      Cache.LOCAL_CACHE[key] = undefined;
+    } else {
+      localStorage.clear();
+      Cache.LOCAL_CACHE = {};
+      Cache.LOCAL_CACHE_SIZE = 0;
+    }
+  }
+}
