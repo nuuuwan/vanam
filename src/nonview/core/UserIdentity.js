33d55a7a6ba3abcde5c770d05766503d6a8172ec
nuuuwan <nuuuwan@gmail.com>
2026-01-16 15:45:07 +0530

    refactor: rename methods for clarity and improve user ID retrieval logic

--- src/nonview/core/UserIdentity.js
+++ src/nonview/core/UserIdentity.js
@@ -1,26 +1,30 @@
 import { v4 as uuidv4 } from "uuid";
+import Cache from "../base/Cache";
 
 export default class UserIdentity {
   static STORAGE_KEY = "vanam_user_id";
   static USER_ID_LENGTH = 8;
-  static instance = null;
 
   constructor(userId) {
     this.userId = userId;
   }
 
-  static getInstanceHot() {
+  static getBrowserUserIdHot() {
     const userId = uuidv4().substring(0, UserIdentity.USER_ID_LENGTH);
-    return new UserIdentity(userId);
+    return userId;
   }
 
-  static getInstance() {
+  static getBrowserUserId() {
     return Cache.get("vanam-user-identity", () =>
       UserIdentity.getInstanceHot()
     );
   }
 
-  static getUserId() {
+  static getInstance() {
+    return new UserIdentity(UserIdentity.getBrowserUserId());
+  }
+
+  getUserId() {
     return this.userId;
   }
 }
