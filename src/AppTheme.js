6fd44d909ebcb2bbeb54b745cc2c0602d4dbe067
nuuuwan <nuuuwan@gmail.com>
2026-05-13 15:52:38 +0530

    fix: update font family to Afacad in index.css and AppTheme, and change stylesheet link in index.html

--- src/AppTheme.js
+++ src/AppTheme.js
@@ -11,7 +11,7 @@ const theme = createTheme({
     },
   },
   typography: {
-    fontFamily: '"Recursive", "Roboto", "Helvetica", "Arial", sans-serif',
+    fontFamily: '"Afacad", "Roboto", "Helvetica", "Arial", sans-serif',
   },
   components: {
     MuiLink: {
