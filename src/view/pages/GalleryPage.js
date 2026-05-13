478c1297c1efebcd685298b3119086faa2826e0d
nuuuwan <nuuuwan@gmail.com>
2026-05-13 11:44:39 +0530

    feat: enhance GalleryPage with photo status indicators and user count

--- src/view/pages/GalleryPage.js
+++ src/view/pages/GalleryPage.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Box, List, CircularProgress, Alert } from "@mui/material";
+import { Box, List, CircularProgress, Alert, Stack, Chip } from "@mui/material";
 import PlantPhotoListItem from "../atoms/PlantPhotoListItem";
 import { useAppBarTitle } from "../../App";
 import { useVanamDataContext } from "../../nonview/core/VanamDataContext";
@@ -35,13 +35,37 @@ const GalleryPage = () => {
     );
   }
 
+  const completedPhotos = plantPhotos.filter((p) => !p.pending);
+  const pendingPhotos = plantPhotos.filter((p) => p.pending);
+  const uniqueUsers = new Set(completedPhotos.map((p) => p.userId)).size;
+
   return (
     <>
+      <Stack direction="row" gap={1} alignItems="center" sx={{ mb: 0.5 }}>
+        {completedPhotos.length > 0 && (
+          <Chip
+            label={`${completedPhotos.length} identified`}
+            size="small"
+            sx={{ bgcolor: "success.main", color: "white" }}
+          />
+        )}
+        {pendingPhotos.length > 0 && (
+          <Chip
+            label={`${pendingPhotos.length} pending`}
+            size="small"
+            sx={{ bgcolor: "warning.main", color: "white" }}
+          />
+        )}
+      </Stack>
       <List>
         {plantPhotos.map((photo) => (
           <PlantPhotoListItem key={photo.imageHash} photo={photo} />
         ))}
       </List>
+      <Alert severity="info" sx={{ mt: 1 }}>
+        This app only displays your plants. The Vanam database has a total of{" "}
+        {completedPhotos.length} plants from {uniqueUsers} users.
+      </Alert>
     </>
   );
 };
