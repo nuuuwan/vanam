b093b86d674f7b4d682c98430c06c811cb829f2e
nuuuwan <nuuuwan@gmail.com>
2026-05-14 09:39:40 +0530

    feat: enhance CustomAppBar with dynamic navigation buttons and state management

--- src/view/moles/CustomAppBar.js
+++ src/view/moles/CustomAppBar.js
@@ -3,12 +3,24 @@ import { AppBar, Toolbar, Typography, IconButton } from "@mui/material";
 import FormatListBulletedIcon from "@mui/icons-material/FormatListBulleted";
 import MapIcon from "@mui/icons-material/Map";
 import NatureIcon from "@mui/icons-material/Nature";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import CustomMenu from "./CustomMenu";
+import { useVanamDataContext } from "../../nonview/core/VanamDataContext";
 
 const CustomAppBar = ({ title }) => {
   const navigate = useNavigate();
+  const location = useLocation();
+  const { plantPhotos } = useVanamDataContext();
   document.title = title;
+
+  const latestImageHash = plantPhotos?.[0]?.imageHash;
+
+  const isPlant = location.pathname.startsWith("/plant/");
+  const isPlants = location.pathname === "/plants";
+  const isMap = location.pathname === "/map";
+
+  const navButtonSx = () => ({});
+
   return (
     <AppBar position="fixed" color="primary" elevation={2}>
       <Toolbar>
@@ -16,13 +28,28 @@ const CustomAppBar = ({ title }) => {
         <Typography variant="body2" sx={{ flexGrow: 1 }}>
           {title}
         </Typography>
-        <IconButton color="inherit" onClick={() => navigate("/plant")}>
+        <IconButton
+          color="inherit"
+          onClick={() => latestImageHash && navigate(`/plant/${latestImageHash}`)}
+          disabled={isPlant || !latestImageHash}
+          sx={navButtonSx(isPlant)}
+        >
           <NatureIcon />
         </IconButton>
-        <IconButton color="inherit" onClick={() => navigate("/plants")}>
+        <IconButton
+          color="inherit"
+          onClick={() => navigate("/plants")}
+          disabled={isPlants}
+          sx={navButtonSx(isPlants)}
+        >
           <FormatListBulletedIcon />
         </IconButton>
-        <IconButton color="inherit" onClick={() => navigate("/map")}>
+        <IconButton
+          color="inherit"
+          onClick={() => navigate("/map")}
+          disabled={isMap}
+          sx={navButtonSx(isMap)}
+        >
           <MapIcon />
         </IconButton>
       </Toolbar>
