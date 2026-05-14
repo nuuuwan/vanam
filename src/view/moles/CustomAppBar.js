619454ba85682972272375f4148dab02c48ae50e
nuuuwan <nuuuwan@gmail.com>
2026-05-14 11:47:40 +0530

    fix: improve text overflow handling in CustomAppBar title

--- src/view/moles/CustomAppBar.js
+++ src/view/moles/CustomAppBar.js
@@ -30,7 +30,7 @@ const CustomAppBar = ({ title }) => {
     <AppBar position="fixed" color="primary" elevation={2}>
       <Toolbar sx={{ maxWidth: 640, mx: "auto", width: "100%" }}>
         <CustomMenu />
-        <Typography variant="body2" sx={{ flexGrow: 1 }}>
+        <Typography variant="body2" sx={{ flexGrow: 1, overflow: "hidden", textOverflow: "ellipsis", whiteSpace: "nowrap" }}>
           {title}
         </Typography>
         <IconButton
