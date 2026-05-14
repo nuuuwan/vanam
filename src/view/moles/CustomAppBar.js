60e001366083a560ed3373fa7e23bd9a690b81de
nuuuwan <nuuuwan@gmail.com>
2026-05-14 09:37:13 +0530

    feat: add NatureIcon button to CustomAppBar for navigation to plant view

--- src/view/moles/CustomAppBar.js
+++ src/view/moles/CustomAppBar.js
@@ -2,6 +2,7 @@ import React from "react";
 import { AppBar, Toolbar, Typography, IconButton } from "@mui/material";
 import FormatListBulletedIcon from "@mui/icons-material/FormatListBulleted";
 import MapIcon from "@mui/icons-material/Map";
+import NatureIcon from "@mui/icons-material/Nature";
 import { useNavigate } from "react-router-dom";
 import CustomMenu from "./CustomMenu";
 
@@ -15,6 +16,9 @@ const CustomAppBar = ({ title }) => {
         <Typography variant="body2" sx={{ flexGrow: 1 }}>
           {title}
         </Typography>
+        <IconButton color="inherit" onClick={() => navigate("/plant")}>
+          <NatureIcon />
+        </IconButton>
         <IconButton color="inherit" onClick={() => navigate("/plants")}>
           <FormatListBulletedIcon />
         </IconButton>
