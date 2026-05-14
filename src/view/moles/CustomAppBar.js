c2cb0753e1d1426f3fcab9702d6f92b68a191cdb
nuuuwan <nuuuwan@gmail.com>
2026-05-14 09:11:20 +0530

    refactor: remove unused Avatar component and navigation logic from CustomAppBar

--- src/view/moles/CustomAppBar.js
+++ src/view/moles/CustomAppBar.js
@@ -1,24 +1,16 @@
 import React from "react";
-import { AppBar, Toolbar, Typography, Avatar } from "@mui/material";
-import { useNavigate } from "react-router-dom";
+import { AppBar, Toolbar, Typography } from "@mui/material";
 import CustomMenu from "./CustomMenu";
 
 const CustomAppBar = ({ title }) => {
-  const navigate = useNavigate();
   document.title = title;
   return (
     <AppBar position="fixed" color="primary" elevation={2}>
       <Toolbar>
-        <Avatar
-          src={`${process.env.PUBLIC_URL}/favicon.ico`}
-          alt="Vanam"
-          onClick={() => navigate("/")}
-          sx={{ width: 32, height: 32, mr: 1.5, cursor: "pointer" }}
-        />
+        <CustomMenu />
         <Typography variant="body2" sx={{ flexGrow: 1 }}>
           {title}
         </Typography>
-        <CustomMenu />
       </Toolbar>
     </AppBar>
   );
