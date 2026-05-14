d4585384b0a1a52274d11a3a5eccb55e6db95575
nuuuwan <nuuuwan@gmail.com>
2026-05-14 11:35:45 +0530

    fix: improve formatting of Box component in PhotoMetadataView for better readability

--- src/view/atoms/PhotoMetadataView.js
+++ src/view/atoms/PhotoMetadataView.js
@@ -12,7 +12,14 @@ const PhotoMetadataView = ({ ut, location, userId }) => {
       sx={{ fontSize: "0.75em", opacity: 0.5 }}
       color="text.secondary"
     >
-      <Box sx={{ display: "inline-flex", alignItems: "center", gap: 1, flexWrap: "wrap" }}>
+      <Box
+        sx={{
+          display: "inline-flex",
+          alignItems: "center",
+          gap: 1,
+          flexWrap: "wrap",
+        }}
+      >
         <DateTimeView ut={ut} />
         <UserView userId={userId} />
       </Box>
